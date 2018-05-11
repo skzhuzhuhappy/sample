dba24700d03b3ca994b353fa36ce9e32ddf93179
vagrant <vagrant@homestead>
2018-05-11 03:32:03 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
 </head>
 <body>
 @include('layouts._header')
